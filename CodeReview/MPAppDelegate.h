504750c3b073a6ab1298e034f649a3b73da2742b
leichunfeng <leicf@zdnst.com>
2014-08-11 23:18:33 +0800

    Initial Commit

---
+++ CodeReview/MPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPAppDelegate.h
+//  CodeReview
+//
+//  Created by leichunfeng on 14-8-11.
+//  Copyright (c) 2014年 zdnst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
